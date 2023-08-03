133550fb25e54f940a44fd7d0dec59d61ff50fc7
Dos <DosDeosos@gmail.com>
2023-08-03 15:30:04 +0700

    add education and skill with styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import { Player } from "@lottiefiles/react-lottie-player";
 import Brain from "./assets/icons/brain.json";
 import Document from "./assets/icons/document.json";
 import Clock from "./assets/icons/clock.json";
+import Airplane from "./assets/icons/Airplane.json";
+import Calendar from "./assets/icons/calendar.json";
+import DegreeCap from "./assets/icons/degreecap.json";
+import Computer from "./assets/icons/computer.json";
+import Hands from "./assets/icons/hands.json";
 
 function App() {
   const [name, setName] = useState("");
@@ -38,6 +43,32 @@ function App() {
     },
   ];
 
+  const Education = [
+    {
+      Icon: Airplane,
+      startyear: 2016,
+      endyear: 2019,
+      degree: "Aviation Management - Bachelor Degree",
+      faculty:
+        "Airport Management Faculty at Civil Aviation Trainer Center (CATC)",
+    },
+  ];
+
+  const TechnicalSkill = [
+    "GIT",
+    "HTML",
+    "CSS",
+    "Tailwind",
+    "Javascript",
+    "React",
+  ];
+  const SoftSkill = [
+    "Empathy",
+    "Responsiveness",
+    "Active Listening",
+    "Ability to work as a group",
+    "Ability to communicate fluently in Thai and proficiently in English",
+  ];
   const setNameFunction = () => {
     const nametouse = nameData[index];
     const character = nametouse.charAt(namelength);
@@ -59,7 +90,7 @@ function App() {
   }, [name]);
 
   return (
-    <div className="resume-container">
+    <div className="resume-container pb-[30px]">
       <div className="grid grid-cols-1 md:grid-cols-2 py-10 px-5">
         <div className="flex flex-col justify-center text-left lg:ps-20 ps-2 pt-5 md:pt-0 text-blue-900">
           <div className="text-[20px]"> Greeting!</div>
@@ -131,7 +162,7 @@ function App() {
           />
         </div>
       </div>
-      <div className="lg:text-[40px] md:text-[30px] text-[25px] text-blue-900 font-semi-bold">
+      <div className="lg:text-[40px] md:text-[30px] text-[25px] text-blue-900 font-semibold">
         About Me
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-5 mt-4">
@@ -148,6 +179,89 @@ function App() {
             </div>
           );
         })}
+      </div>
+      <div className="lg:text-[40px] md:text-[30px] text-[25px] text-blue-900 font-semibold mt-4">
+        Education
+      </div>
+      <div className="grid grid-cols-1 gap-8 px-5 mt-4">
+        {Education.map((data, index) => {
+          return (
+            <div key={index} className="Card-Education w-fit mx-auto">
+              <Player
+                src={data.Icon}
+                className="md:w-[80px] md:h-[80px] w-[50px] h-[50px]"
+                autoplay
+                loop
+              />
+              <div className="text-left w-fit mx-auto">
+                <div className="flex items-center gap-2">
+                  <Player
+                    src={Calendar}
+                    className="md:w-[50px] md:h-[50px] w-[40px] h-[40px]"
+                    autoplay
+                    loop
+                  />
+                  Starting Year: {data.startyear} <br /> Graduation Year:{" "}
+                  {data.endyear}
+                </div>
+                <div className="flex items-center gap-2">
+                  <Player
+                    src={DegreeCap}
+                    className="md:w-[50px] md:h-[50px] w-[40px] h-[40px]"
+                    autoplay
+                    loop
+                  />
+                  Degree: {data.degree}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="lg:text-[40px] md:text-[30px] text-[25px] text-blue-900 font-semibold mt-4">
+        Skills
+      </div>
+      <div className="grid grid-cols-1 gap-8 px-5 mt-4 md:grid-cols-2">
+        <div className="Card-TechnicalSkill">
+          <div className="lg:text-[40px] md:text-[30px] text-[25px] text-blue-900 font-medium mt-4">
+            Technical Skills
+          </div>
+          <Player
+            src={Computer}
+            className="md:w-[120px] md:h-[120px] w-[80px] h-[80px]"
+            autoplay
+            loop
+          />
+          <ul className="list-disc ml-[46%]">
+            {TechnicalSkill.map((data, index) => {
+              return (
+                <li key={index} className="w-fit">
+                  {data}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="Card-SoftSkill">
+          <div className="lg:text-[40px] md:text-[30px] text-[25px] text-blue-900 font-medium mt-4">
+            Soft Skills
+          </div>
+          <Player
+            src={Hands}
+            className="md:w-[120px] md:h-[120px] w-[80px] h-[80px]"
+            autoplay
+            loop
+          />
+          <ul className="list-disc ml-[10%]">
+            {SoftSkill.map((data, index) => {
+              return (
+                <li key={index} className="w-fit text-start">
+                  {data}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
